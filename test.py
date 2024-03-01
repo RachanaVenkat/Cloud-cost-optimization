c5c6a9956e15d684bde55a101d29e1234f305a19
“Rachana” <rachanaa.v.01@gmail.com>
2024-03-01 15:11:53 +0530

    created an s3 bucket on aws

--- test.py
+++ test.py
@@ -1,17 +1,1 @@
-import boto3
-
-# Create an S3 client
-client = boto3.client('s3')
-
-# Create the bucket
-response = client.get_bucket_acl(
-    Bucket='rachana-boto3-demo-789'
-)
-print(response)
-# response = client.create_bucket(
-#     Bucket='rachana-boto3-demo-789',
-#     CreateBucketConfiguration={
-#         'LocationConstraint': 'ap-south-1'
-#     },
-# )
 
